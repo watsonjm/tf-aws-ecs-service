490f713df8cbdc3832bc96ef4aa57314c80c2f53
John Watson <jwatson@ksmconsulting.com>
2021-05-14 14:57:53 -0400

    added ecs_service to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "discovery_service" {
   value = aws_service_discovery_service.this
 }
 #"${module.ecs_middleware.discovery_service[0].name}.${aws_service_discovery_private_dns_namespace.ecs.name}"
+output "ecs_service" {
+  value = aws_ecs_service.this
+}
